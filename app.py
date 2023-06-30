6c387c6d3170fc875d8bfe36a808268d43745a6a
Brianne <bcwilhelmi@gmail.com>
2023-06-30 15:50:49 -0700

    committing after step 8, using session

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, flash, jsonify
+from flask import Flask, request, render_template, redirect, flash, session
 from surveys import satisfaction_survey as survey
 from random import randint,  choice, sample
 from flask_debugtoolbar import DebugToolbarExtension
@@ -10,7 +10,7 @@ app.config['SECRET_KEY'] = "secretkey"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app)
 
-responses = []
+session_responses = "responses"
 
 
 @app.route('/')
@@ -19,12 +19,21 @@ def home_page():
     return render_template('index.html', survey=survey)
 
 
+@app.route('/start', methods=['POST'])
+def set_responses():
+    """Sets session['responses'] to an empty list"""
+    session[session_responses] = []
+    return redirect('/questions/0')
+
+
 @app.route('/answer', methods=['POST'])
 def handle_question():
     """Saves answer and redirects to next question, or completes the survey"""
     choice = request.form['answer']
 
+    responses = session.get('responses', [])
     responses.append(choice)
+    session['responses'] = responses
 
     if (len(responses) == len(survey.questions)):
         return redirect('/complete')
@@ -36,6 +45,8 @@ def handle_question():
 @app.route('/questions/<int:question_num>')
 def show_question(question_num):
     """Current Question"""
+
+    responses = session.get(session_responses)
 
     if (len(responses) == len(survey.questions)):
         return redirect('/complete')
